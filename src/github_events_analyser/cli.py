7b580ff145d8450888dc3712ca1bbaaea420e0f5
ZakYeo <zakyeomanson@gmail.com>
2023-06-23 22:53:12 +0100

    Added get_repo and documentation

--- src/github_events_analyser/cli.py
+++ src/github_events_analyser/cli.py
@@ -16,6 +16,12 @@ def user_events(args):
     print(events)
 
 
+def repo_events(args):
+    api = GitHubAPI(args.token)
+    events = api.get_repo_events(args.username, args.repo, args.event_type)
+    print(events)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Interact with GitHub API.')
     parser.add_argument('--token', required=True, help='Your GitHub token')
@@ -30,6 +36,14 @@ def main():
         'username', help='GitHub username to fetch events for')
     parser_user_events.add_argument('--event-type', help='Type of events to fetch')
     parser_user_events.set_defaults(func=user_events)
+
+    parser_repo_events = subparsers.add_parser('repo-events', help='Fetch repo events')
+    parser_repo_events.add_argument(
+        'username', help='GitHub username to fetch events for')
+    parser_repo_events.add_argument(
+        'repo', help='GitHub repository to fetch events for')
+    parser_repo_events.add_argument('--event-type', help='Type of events to fetch')
+    parser_repo_events.set_defaults(func=repo_events)
 
     args = parser.parse_args()
     args.func(args)
--- src/github_events_analyser/github_api.py
+++ src/github_events_analyser/github_api.py
@@ -4,25 +4,86 @@ import requests
 
 
 class GitHubAPI:
+    """
+    A wrapper class for the GitHub API.
+
+    Attributes:
+        BASE_URL (str): The base URL for the GitHub API.
+    """
+
     BASE_URL = 'https://api.github.com'
 
     def __init__(self, token):
+        """
+        Initializes the GitHubAPI class with an authentication token.
+
+        Args:
+            token (str): The GitHub API token.
+        """
         self.headers = {
             'Authorization': f'token {token}',
             'Accept': 'application/vnd.github.v3+json'
         }
 
     def get_user_info(self, username):
+        """
+        Fetches information about a GitHub user.
+
+        Args:
+            username (str): The GitHub username.
+
+        Returns:
+            dict: The user's information.
+        """
         response = requests.get(
             f'{self.BASE_URL}/users/{username}', headers=self.headers)
         return response.json()
 
     def get_user_events(self, username, event_type=None):
+        """
+        Fetches events associated with a GitHub user.
+
+        Args:
+            username (str): The GitHub username.
+            event_type (str, optional): The type of event to fetch.
+
+        Returns:
+            list: The user's events.
+        """
         response = requests.get(
             f'{self.BASE_URL}/users/{username}/events', headers=self.headers)
         events = response.json()
 
+        # If an event type is specified, filter the events
         if event_type:
             events = [event for event in events if event['type'] == event_type]
 
         return events
+
+    def get_repo_events(self, username, repo, event_type=None):
+        """
+        Fetch and display the latest events for a given GitHub repository
+        Filter events using event_type, e.g y "PushEvent" or "PullRequestEvent"
+
+        TODO:
+            - Calculate and display statistics on the number of events per type
+            - Identify the most active GitHub user based on the number of events
+            - Implement error handling for API requests and display appropriate
+            error messages
+            - Use pagination to handle a large number of events and enable the
+            retrieval of more events beyond the initial limit.
+            - Implement sorting options to display events in chronological or
+            reverse-chronological order.
+
+        Args:
+            username (str): The owner of the repository.
+            repo (str): The repository name.
+            event_type (str, optional): The type of event to fetch.
+
+        Returns:
+            list: The repository's events.
+        """
+        url = f'{self.BASE_URL}/repos/{username}/{repo}/events'
+        response = requests.get(
+            url, headers=self.headers)
+        return response.json()
